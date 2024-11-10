d984df2f3dda677472d8b15c1b9a4ae3cc9664b9
wenjin <hewenjin94@outlook.com>
2024-11-11 00:34:46 +0800

    fix: expand user home error

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 ClientId = "347227d5-402d-47b5-bcfe-ef427563b99e"
 ClientSecret = ""
 Scopes = ['https://outlook.office.com/IMAP.AccessAsUser.All','https://outlook.office.com/SMTP.Send']
-RefreshTokenFileName = "~/.cache/neomutt/imap_smtp_refresh_token"
-AccessTokenFileName = "~/.cache/neomutt/imap_smtp_access_token"
+RefreshTokenFileName = os.path.expanduser('~') + "/.cache/neomutt/imap_smtp_refresh_token"
+AccessTokenFileName = os.path.expanduser('~') + "/.cache/neomutt/imap_smtp_access_token"
 
 # Optionally specify a tenantId here: "https://login.microsoftonline.com/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"
 Authority = None 

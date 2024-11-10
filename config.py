19f7e3646123926c88f2cd56db4f910fd283f5e7
wenjin <hewenjin94@outlook.com>
2024-11-11 00:01:31 +0800

    feat: adjust tokenfile location

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 ClientId = "347227d5-402d-47b5-bcfe-ef427563b99e"
 ClientSecret = ""
 Scopes = ['https://outlook.office.com/IMAP.AccessAsUser.All','https://outlook.office.com/SMTP.Send']
-RefreshTokenFileName = "imap_smtp_refresh_token"
-AccessTokenFileName = "imap_smtp_access_token"
+RefreshTokenFileName = "~/.cache/neomutt/imap_smtp_refresh_token"
+AccessTokenFileName = "~/.cache/neomutt/imap_smtp_access_token"
 
 # Optionally specify a tenantId here: "https://login.microsoftonline.com/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"
 Authority = None 
